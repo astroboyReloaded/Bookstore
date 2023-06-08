b7d951a4341fd3e1ddeb2cdd752430ef7071fb26
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-06-08 16:52:48 -0600

    Add list to render individual book

--- src/components/Books.js
+++ src/components/Books.js
@@ -5,9 +5,25 @@ const Books = () => {
   const [bookList] = useState(JSON.parse(localStorage.getItem('books')) || defaultBooks);
 
   return (
-    <header className="App-header">
-      <h1>Books</h1>
-    </header>
+    <>
+      <header>
+        <h1>Books</h1>
+      </header>
+      <main>
+        <ul>
+          {bookList.map((book) => (
+            <li key={book.title}>
+              <article>
+                <span>{book.category}</span>
+                <h1>{book.title}</h1>
+                <data>{book.author}</data>
+                
+              </article>
+            </li>  
+          ))}
+        </ul>
+      </main>
+    </>
   )
 };
 
