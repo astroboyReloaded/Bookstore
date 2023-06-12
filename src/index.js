bc685574e357fcebb8586d3d68024fd644b4c6a4
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-06-12 14:13:54 -0600

    Fix eslint error

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import store from './redux/store';
 import { Provider } from 'react-redux';
+import store from './redux/store';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
