2fb1352a0e8326d33d0ff5ca13752f96d3518c3e
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-06-12 13:15:01 -0600

    Create booksSlice.js

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -7,6 +7,18 @@ const initialState = {
 const booksSlice = createSlice({
   name: 'books',
   initialState,
+  reducers: {
+    addBook: (state, { payload }) => ({
+      ...state,
+      books: payload,
+    }),
+    removeBook: (state, { payload }) => ({
+      ...state,
+      books: state.books.filter((book) => book.id !== payload),
+    }),
+  },
 });
+
+export const { addBook, removeBook } = booksSlice.actions;
 
 export default booksSlice.reducer;
